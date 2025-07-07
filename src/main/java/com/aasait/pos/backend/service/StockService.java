10aba77fdbedc1e973cfe421397ec14af323ec25
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-07-07 23:26:28 +0530

    Supllier order add service done

--- src/main/java/com/aasait/pos/backend/service/StockService.java
+++ src/main/java/com/aasait/pos/backend/service/StockService.java
@@ -2,6 +2,7 @@ package com.aasait.pos.backend.service;
 
 import com.aasait.pos.backend.dto.request.AddItemDTO;
 import com.aasait.pos.backend.dto.request.AddSupplierDTO;
+import com.aasait.pos.backend.dto.request.SupplierOrderDTO;
 import com.aasait.pos.backend.dto.response.SupplierDTO;
 
 import java.util.List;
@@ -16,4 +17,6 @@ public interface StockService {
     List<String> getAllSupplierNames();
 
     List<String> getAllItemsName();
+
+    void saveSupplierOrder(SupplierOrderDTO supplierOrderDTO);
 }
