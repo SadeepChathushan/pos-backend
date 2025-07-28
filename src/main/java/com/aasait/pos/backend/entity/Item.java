db67e42b1703778a1115c9a84b34be7dfc14b4e7
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-07-28 07:49:27 +0530

    changes

--- src/main/java/com/aasait/pos/backend/entity/Item.java
+++ src/main/java/com/aasait/pos/backend/entity/Item.java
@@ -2,6 +2,7 @@ package com.aasait.pos.backend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
