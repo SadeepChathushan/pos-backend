f523a170eb50f138e330ddf64020628098e6ff8b
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-07-29 11:31:03 +0530

    Service type create

---
+++ src/main/java/com/aasait/pos/backend/service/IMPL/StockkeeperIMPL.java
@@ -0,0 +1,8 @@
+package com.aasait.pos.backend.service.IMPL;
+
+import com.aasait.pos.backend.service.StockkeeperService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockkeeperIMPL implements StockkeeperService {
+}
