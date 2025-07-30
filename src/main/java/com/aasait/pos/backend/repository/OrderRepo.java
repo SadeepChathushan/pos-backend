91640f64aaadb94839ac29a619c85caaa3c579b4
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2025-07-30 10:16:57 +0530

    Some updates in repo and entity

---
+++ src/main/java/com/aasait/pos/backend/repository/OrderRepo.java
@@ -0,0 +1,9 @@
+package com.aasait.pos.backend.repository;
+
+import com.aasait.pos.backend.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order,Long> {
+}
